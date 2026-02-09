dd7f90d34482b7d22436fa85578006eb2658a8b6
almacode1703 <testmygitt.2023@gmail.com>
2026-02-10 01:26:50 +0530

    updates

---
+++ Module_1/Lesson5/summer.py
@@ -0,0 +1,8 @@
+# take temperature input
+temperature = float(input("Enter the temperature: "))
+
+# decision using condition
+if temperature >= 25:
+    print("You can wear light clothes.")
+else:
+    print("Wear a jacket or pullover.")

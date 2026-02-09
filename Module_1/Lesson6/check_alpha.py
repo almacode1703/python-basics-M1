187d28cf03eac3931821843e4d5ef4bf955d2fea
almacode1703 <testmygitt.2023@gmail.com>
2026-02-10 01:33:48 +0530

    updates

---
+++ Module_1/Lesson6/check_alpha.py
@@ -0,0 +1,8 @@
+# take input
+ch = input("Enter a character: ")
+
+# check condition
+if ('a' <= ch <= 'z') or ('A' <= ch <= 'Z'):
+    print("It is an alphabet.")
+else:
+    print("It is not an alphabet.")
